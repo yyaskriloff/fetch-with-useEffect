e80c4edf0936376a630244bbc7d6f148d06e2063
Aaron Skriloff <69407772+yyaskriloff@users.noreply.github.com>
2025-11-09 09:25:56 +0200

    init server

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hello World");
+import http from "node:http";
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello World");
+});
+
+server.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
