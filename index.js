424b74f0369b81d3b005986b0eeb35e2cca43f35
Aaron Skriloff <69407772+yyaskriloff@users.noreply.github.com>
2025-11-09 09:30:32 +0200

    implement people api

--- index.js
+++ index.js
@@ -1,8 +1,45 @@
 import http from "node:http";
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello World");
+const people = {
+  1: {
+    firstName: "John",
+    lastName: "Doe",
+    age: 20,
+    city: "New York",
+    country: "United States",
+    email: "john.doe@example.com",
+    phone: "1234567890",
+    website: "https://www.john.com",
+  },
+  2: {
+    firstName: "Jane",
+    lastName: "Doe",
+    age: 21,
+    city: "Los Angeles",
+    country: "United States",
+    email: "jane.doe@example.com",
+    phone: "1234567890",
+    website: "https://www.jane.com",
+  },
+};
+
+const server = http.createServer(async (req, res) => {
+  if (req.url === "/api/people/1") {
+    const person = people[1];
+    await new Promise((resolve) => setTimeout(resolve, 1000 * 10));
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(person));
+    return;
+  } else if (req.url === "/api/people/2") {
+    const person = people[2];
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(person));
+    return;
+  } else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("Not Found");
+    retuern;
+  }
 });
 
 server.listen(3000, () => {
